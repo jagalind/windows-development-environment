38a7abe6d905d14b96884696f5fd47170d502914
Javier Galindo <javier.galindo@gmail.com>
2019-01-24 08:13:12 -0800

    Initial changes from Fork

--- env-windows.ps1
+++ env-windows.ps1
@@ -7,9 +7,7 @@
 #                                                                               #
 #-------------------------------------------------------------------------------#
 
-#
 # Functions
-#
 
 function Update-Environment-Path
 {
@@ -17,211 +15,77 @@ function Update-Environment-Path
         + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
-#
 # Package Managers
-#
 
 # Choco
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+# Create PowerShell profile file before installing choco. 
+if (-Not (Test-Path $profile -PathType Leaf))
+{
+    New-Item -Path $profile -ItemType "file" -Force
+}
+
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 Update-Environment-Path
 
-#
-# Git
-#
+# Choco Core Extenstions
+choco install chocolatey-core.extension
 
-choco install git --yes
+# Git
+choco install git.install --params "/GitAndUnixToolsOnPath /NoAutoCrlf" --yes
 choco install tortoisegit --yes
 Update-Environment-Path
-git config --global alias.pom 'pull origin master'
-git config --global alias.last 'log -1 HEAD'
-git config --global alias.ls "log --pretty=format:'%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short"
-git config --global alias.ammend "commit -a --amend"
-git config --global alias.standup "log --since yesterday --author $(git config user.email) --pretty=short"
-git config --global alias.everything "! git pull && git submodule update --init --recursive"
-git config --global alias.aliases "config --get-regexp alias"
+# git config --global alias.pom 'pull origin master'
+# git config --global alias.last 'log -1 HEAD'
+# git config --global alias.ls "log --pretty=format:'%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]' --decorate --date=short"
+# git config --global alias.ammend "commit -a --amend"
+# git config --global alias.standup "log --since yesterday --author $(git config user.email) --pretty=short"
+# git config --global alias.everything "! git pull && git submodule update --init --recursive"
+# git config --global alias.aliases "config --get-regexp alias"
+# Update-Environment-Path
 
-#
-# AWS awscli
-#
-choco install awscli --yes
-Update-Environment-Path
+# C/C++ Languages
+choco install VisualStudio2015Community --yes --timeout 0 -package-parameters "--AdminFile .\tools_msvc2015AdminDeployment.xml"
 
-#
-# MinGW
-# 
-
-choco install mingw --yes
-Update-Environment-Path
-
-# Get-Command mingw32-make
-
-# todo: Alias `make` to `mingw32-make` in Git Bash
-# todo: Write `mingw32-make %*` to make.bat in MinGW install directory
-
-#
-# Caddy HTTP Server
-#
-
-choco install caddy --yes
-Update-Environment-Path
-
-#
-# Languages
-#
-choco install php --yes
-choco install ruby --yes
-choco install ruby2.devkit --yes
-choco install python2 --yes
-choco install jdk8 --yes
-Update-Environment-Path
-
-# Node
-choco install nodejs.install --yes
-Update-Environment-Path
-npm install --global --production npm-windows-upgrade
-npm-windows-upgrade --npm-version latest
-npm install -g gulp-cli 
-npm install -g yo
-npm install -g mocha
-npm install -g install-peerdeps
-npm install -g typescript
-# npm install prettier-eslint --save-dev
-
-#
-# Docker
-# 
-
-# Hyper-V required for docker and other things
-Enable-WindowsOptionalFeature -Online -FeatureName:Microsoft-Hyper-V -All -NoRestart
-
-choco install docker --yes
-choco install docker-machine --yes
-choco install docker-compose --yes
-choco install docker-for-windows --yes
-
-Update-Environment-Path
-
-docker pull worpress
-docker pull mysql
-docker pull phpmyadmin
-
-
-# Yarn
-# ?? choco install yarn --yes
-
-# Bower
-npm install -g bower
-
-# Grunt
-npm install -g grunt-cli
-
-# ESLint
-npm install -g eslint
-npm install -g babel-eslint
-npm install -g eslint-plugin-react
-npm install -g install-peerdeps
-install-peerdeps --dev eslint-config-airbnb
-
-#
 # VS Code
-#
-
-choco install visualstudiocode --yes # includes dotnet
+choco install vscode --yes
+choco install vscode-icons --yes
+choco install vscode-gitlens --yes
 Update-Environment-Path
-code --install-extension robertohuertasm.vscode-icons
-code --install-extension CoenraadS.bracket-pair-colorizer
 
-# PowerShell support
+code --install-extension huizhou.githd
+code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.PowerShell
 
-# CSharp support
-code --install-extension ms-vscode.csharp
-
-# PHP support
-code --install-extension felixfbecker.php-debug
-code --install-extension HvyIndustries.crane
-
-# Ruby support
-code --install-extension rebornix.Ruby
-
-# HTML, CSS, JavaScript support
-code --install-extension Zignd.html-css-class-completion
-code --install-extension lonefy.vscode-JS-CSS-HTML-formatter
-code --install-extension robinbentley.sass-indented
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension RobinMalfait.prettier-eslint-vscode
-code --install-extension flowtype.flow-for-vscode
-code --install-extension dzannotti.vscode-babel-coloring
-code --install-extension esbenp.prettier-vscode
-code --install-extension formulahendry.auto-rename-tag
-
-# NPM support
-code --install-extension eg2.vscode-npm-script
-code --install-extension christian-kohler.npm-intellisense
-
-# Mocha support
-code --install-extension spoonscen.es6-mocha-snippets
-code --install-extension maty.vscode-mocha-sidebar
-
-# React Native support
-code --install-extension vsmobile.vscode-react-native
-npm install -g create-react-native-app
-npm install -g react-native-cli
-
-# Docker support
-code --install-extension PeterJausovec.vscode-docker
-
-# PlantUML support
-code --install-extension jebbs.plantuml
-
-#
-# MySQL
-#
-
-choco install mysql --yes
-choco install mysql.workbench --yes
-
-
-#
-# Android Studio
-# 
-
-choco install androidstudio --yes
-
-#
-# Static Site Generators
-#
-
-# Hugo
-choco install hugo --yes
-
-#
-# Basic Utilities
-#
-
-# choco install slack --yes
-# choco install xenulinksleuth --yes
+# VirtualBox
+choco install virtualbox --yes
 
 # File Management
-choco install beyondcompare --yes
-choco install 7zip --yes
-choco install filezilla --yes
-choco install dropbox --yes
+choco install cmder --yes
+choco install meld --yes
+choco install 7zip.install --yes
+choco install notepadplusplus.install --yes
 
 # Media Viewers
-choco install irfanview --yes
 choco install vlc --yes
 
 # Browsers
 choco install googlechrome --yes
-choco install firefox --yes
 
 # Misc
 choco install sysinternals --yes
-choco install procexp --yes
-choco install awscli --yes
-choco install firacode --yes # See https://www.youtube.com/watch?v=KI6m_B1f8jc
+choco install adobereader --yes
+choco install SkypeForBusinessBasic --yes
+choco install teraterm --yes
+choco install MobaXTerm --yes
+# choco install slack --yes
+
+# FTDI Drivers - Checksum Fails - Ignore for now
+choco install ftdi-drivers --ignore-checksums --yes 
+
+# MS Office 2018 - Stil pre-production
+choco install --yes office2019-proplus --pre
 
 Update-Environment-Path
 
